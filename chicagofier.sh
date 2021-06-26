c1085df0a9075d2edeb8708cbba86b12fa6e151e
dominichayesferen <ferenosdev@outlook.com>
2021-06-26 04:04:54 +0100

    ...

--- chicagofier.sh
+++ chicagofier.sh
@@ -1,5 +1,44 @@
 #!/bin/bash
 
+if [ "$1" == "--userland" ]; then
+    if [ ! -d "$2" ]; then
+        echo "No. There is no folder here."
+        exit 1
+    fi
+    
+    rm -rf /home/$useris/.themes/Chicago95
+    while [ ! -d /home/$useris/.themes/Chicago95 ]; do
+        python3 ./installer.py &
+        sleep 10
+        read -p "
+
+    Install the theme. Yes.
+    Press ENTER here when it's done installing and you see that random instruction manual on screen. Don't close the instructions manual, though.
+    "
+        if [ ! -d /home/$useris/.themes/Chicago95 ]; then
+            clear
+            echo "You cheated not only the script, but yourself.
+
+    You didn't grow.
+    You didn't Chicagofy.
+    You tried to troll the script and gained nothing.
+
+    You wasted your time.
+    Nothing was done and nothing was gained.
+
+    It's sad that you even bothered trying this.
+    Now just install the theme already."
+        fi
+    done
+
+    clear
+    echo "User configs go brrr..."
+    xfconf-query -c xsettings -p /Gtk/DialogsUseHeader -s false
+    
+    exit 0
+    
+fi
+
 if [ "$(whoami)" != "root" ]; then
     echo "Type 'sudo !!' below and press ENTER. Trust me, it makes the script go brrr."
     exit 1
@@ -42,39 +81,10 @@ sed -i 's%@import url("gtk-headerbars.css");%/*@import url("gtk-headerbars.css")
 sed -i 's%/*@import url("gtk-headerbars_no-csd.css");*/%@import url("gtk-headerbars_no-csd.css");%g' Theme/Chicago95/gtk-3.0/gtk.css
 clear
 
-rm -rf /home/$useris/.themes/Chicago95
-while [ ! -d /home/$useris/.themes/Chicago95 ]; do
-    sudo --user=$useris python3 ./installer.py &
-    sleep 10
-    read -p "
-
-Install the theme. Yes.
-Press ENTER here when it's done installing and you see that random instruction manual on screen. Don't close the instructions manual, though.
-"
-    if [ ! -d /home/$useris/.themes/Chicago95 ]; then
-        clear
-        echo "You cheated not only the script, but yourself.
-
-You didn't grow.
-You didn't Chicagofy.
-You tried to troll the script and gained nothing.
-
-You wasted your time.
-Nothing was done and nothing was gained.
-
-It's sad that you even bothered trying this.
-Now just install the theme already."
-    fi
-done
-
 clear
 echo "Installing stuff system-wide go brrr..."
 cp -Rfv ./Theme/Chicago95 /usr/share/themes/
 cp -Rfv ./Icons/Chicago95 /usr/share/icons/
-
-clear
-echo "User configs go brrr..."
-sudo --user=$useris xfconf-query -c xsettings -p /Gtk/DialogsUseHeader -s false
 
 clear
 echo "Installing a better browser that doesn't look like s***..."
